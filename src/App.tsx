1fbb4968331e23bd1f2e70eb907b98a681fd7c48
Erez.Gavish <erezg3@gmail.com>
2025-09-21 16:50:59 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {FractalsPlayground} from './components/FractalsPlayground';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <FractalsPlayground />
+    </div>
+  );
+};
+
+export default App;
