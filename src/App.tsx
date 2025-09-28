8f86ffec6dd24ae2e059e3cbe1b57c5f6728c5c3
er-gv <erezg3@gmail.com>
2025-09-28 03:22:53 +0300

    added firebase support

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {FractalsPlayground} from './components/fractalsPlayground';
 import './App.css';
+import './config/firebase.config';
+
+
+
 
 const App: React.FC = () => {
   return (
